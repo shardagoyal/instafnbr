967603328f5649a693aea60c79c1ec928d3a9e72
shardagoyal <goyalsharda258@gmail.com>
2017-07-12 16:33:00 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-from os import environ
-from flask import Flask
+import flask
+app = flask.Flask(__name__)
 
-app = Flask(__name__)
-app.run(environ.get('PORT'))
+@app.route("/")
+def index():
+    #do whatevr here...
+    return "Hello Heruko"
